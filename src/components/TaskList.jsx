9671fc9c2035ed2bdbc39067f0bd46fa82d95918
anna-sahradyan <a.sahradyan72@gmail.com>
2023-04-24 21:50:42 +0400

    changes

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -4,7 +4,7 @@ import Task from "./Task";
 import {toast} from "react-toastify";
 import axios from "axios";
 //import {URL} from "../App.js";
-import {BASE_URL} from "./api/helper";
+import {BASE_URL} from "./api";
 import Loading from "./Loading";
 
 
